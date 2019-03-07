36fa93b699381b2eca58b37a82d70580ae8ab1d2
Marc Raphael <marc.raphael12@gmail.com>
2019-03-07 10:58:23 -0800

    end

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -12,14 +12,17 @@ function initializePage() {
 
 // Function that is called when the server responds to the AJAX call
 function onServerResponse(data){
+	console.log(data.id);
     var labElementId = getLabElementId(data.id);
+    console.log(labElementId);
     var labElement = $(labElementId);
+    console.log(labElement);
     labElement.css({'background': 'yellow'});
 }
 
 
 // Function to format the CSS selector
 function getLabElementId(id){
-    var labElementId = '#lab_' + id;
+    var labElementId = '#lab-' + id;
     return labElementId;
 }
